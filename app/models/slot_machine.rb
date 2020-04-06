9421b1c69ec568ed3a14951576f83e02288f36d3
theowiik <42991003+theowiik@users.noreply.github.com>
2020-04-06 20:44:47 +0200

    Start of creating slot machine logic

--- app/models/slot_machine.rb
+++ app/models/slot_machine.rb
@@ -1,5 +1,22 @@
 class SlotMachine < ApplicationRecord
-  def spin
-    5
+  # Spins the slot machine, and returns the grid of symbols.
+  # TODO: Represent as rows or reels?
+  #
+  # @return [List[List[Int]]] the grid of symbols.
+  def random_grid
+    [
+      [1, 3, 4],
+      [1, 3, 4],
+      [1, 3, 4]
+    ]
+  end
+
+  # Given a grid of symbols and a bet, returns how much winnings there are.
+  #
+  # @param [List[List[Int]]] the grid of symbols.
+  # @param []
+  # @return [Int] the winnings.
+  def reels_to_winnings(grid, bet)
+    return 10 * bet
   end
 end
