6661391bea31ad51adcddbf2f641945a7c67c5fe
theowiik <42991003+theowiik@users.noreply.github.com>
2020-04-10 11:09:43 +0200

    Add logic to check if one can spin a slot machine

--- app/controllers/api/v1/slot_machines_controller.rb
+++ app/controllers/api/v1/slot_machines_controller.rb
@@ -5,12 +5,49 @@ module Api
     # The controller for slot machines.
     class SlotMachinesController < ApplicationController
       def index
-        render json: SlotMachine.all.sample.slot_machine_symbols
+        render json: SlotMachine.all
       end
 
       def spin
-        slot_machine = SlotMachine.find(params[:id])
-        render json: slot_machine.random_grid
+        bet = 10                # will be given by params
+        user = User.all.sample  # sample user
+        no_bet = false          # if no bet was provided
+
+        if bet <= 0 || no_bet
+          return render json:
+            {
+              status: 400,
+              error: {
+                message: 'Invalid bet: bet must be greater than 0.'
+              }
+            }
+        end
+
+        begin
+          if user.decrement(:balance, bet).save
+            slot_machine = SlotMachine.find(params[:id])
+            return render json: {
+              status: 200,
+              matrix: slot_machine.random_grid,
+              balance: user.balance,
+              payout: 1000
+            }
+          end
+        rescue
+          return render json: {
+            status: 400,
+            error: {
+              message: "Insufficient funds: you can't afford playing this. Poor person. lol"
+            }
+          }
+        end
+
+        render json: {
+          status: 500,
+          error: {
+            message: 'You should not be here.'
+          }
+        }
       end
     end
   end
