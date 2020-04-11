8aa48a25d1236116017e2135443b18ef79cfbaa5
theowiik <42991003+theowiik@users.noreply.github.com>
2020-04-11 09:46:38 +0200

    Update schema to use structure.sql instead. And move balance >= 0 constraint to database.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,2 @@
 class User < ApplicationRecord
-  # TODO: Move constraint to database.
-  validates :balance, numericality: { greater_than_or_equal_to: 0 }
 end
--- db/migrate/20200409164138_add_balance_constraint.rb
+++ db/migrate/20200409164138_add_balance_constraint.rb
@@ -1,11 +1,9 @@
 class AddBalanceConstraint < ActiveRecord::Migration[6.0]
-  def change; end
+  def up
+    execute 'ALTER TABLE users ADD CONSTRAINT non_negative_balance CHECK(balance >= 0);'
+  end
 
-  # def up
-  #   execute 'ALTER TABLE users ADD CONSTRAINT non_negative_balance CHECK(balance >= 0);'
-  # end
-
-  # def down
-  #   execute 'ALTER TABLE users DROP CONSTRAINT non_negative_balance;'
-  # end
+  def down
+    execute 'ALTER TABLE users DROP CONSTRAINT non_negative_balance;'
+  end
 end
