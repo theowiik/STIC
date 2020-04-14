f56313421fe3763f1291c7e9e57fa5fef05d89ba
theowiik <42991003+theowiik@users.noreply.github.com>
2020-04-14 14:52:39 +0200

    Update prints in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,32 @@ puts("\nSeeding!")
 
 n = 10
 
-puts("Created #{n} slotmachines...")
+puts("Creating #{n} slotmachines...")
 n.times do
   SlotMachine.create(name: Faker::Hacker.ingverb, rows: 3, columns: 3)
 end
 
-puts("Created #{n} symbols for each slot machine...")
+x = 3
+
+chars = ('a'..'z').to_a
+
+puts("Creating #{x} symbols for each slot machine...")
 SlotMachine.all.each do |slot_machine|
-  n.times do
+  i = 0
+  x.times do
     SlotMachineSymbol.create(
       name: Faker::SlackEmoji.emoji,
-      slot_machine: slot_machine
+      slot_machine: slot_machine,
+      ascii: chars[i]
     )
+    i += 1
   end
 end
 
-puts('Created a sample user...')
-User.create(name: 'Joe Danger', email: 'joe@danger.com')
+puts('Creating a sample user...')
+User.create(name: 'Joe Danger', email: 'joe@danger.com', balance: 1_000)
 
-puts('Create some lines for 3 * 3 machines...')
+puts('Creating lines for 3 * 3 machines...')
 bitmaps = %w[111000000 000111000 000000111]
 bitmaps.each do |bitmap|
   Line.create(rows: 3, columns: 3, bitmap: bitmap)
