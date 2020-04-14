260ff179658a24a8e8f19526bec54b246dadf7d5
theowiik <42991003+theowiik@users.noreply.github.com>
2020-04-14 11:06:55 +0200

    Create a generator for errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def generate_error(status, msg)
+    render json: {
+      status: status,
+      error: {
+        message: msg
+      }
+    }
+  end
 end
