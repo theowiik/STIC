0ed8265762050f777f3dd632d2379e0601bc18e3
theowiik <42991003+theowiik@users.noreply.github.com>
2020-04-14 19:11:55 +0200

    Add more bitmaps to seeds.rb and paytables

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,18 @@ puts("Creating #{x} symbols for each slot machine...")
 SlotMachine.all.each do |slot_machine|
   i = 0
   x.times do
-    SlotMachineSymbol.create(
+    symbol = SlotMachineSymbol.create(
       name: Faker::SlackEmoji.emoji,
       slot_machine: slot_machine,
       ascii: chars[i]
     )
+
+    SlotMachineSymbolPaytable.create(
+      slot_machine_symbol: symbol,
+      occurrences: 3,
+      pay: 50
+    )
+
     i += 1
   end
 end
@@ -28,7 +35,21 @@ puts('Creating a sample user...')
 User.create(name: 'Joe Danger', email: 'joe@danger.com', balance: 1_000)
 
 puts('Creating lines for 3 * 3 machines...')
-bitmaps = %w[111000000 000111000 000000111]
+bitmaps = %w[
+  111000000
+  000111000
+  000000111
+  101010000
+  000101010
+  100010001
+  001010100
+  001110000
+  000110001
+  101000010
+  010000101
+  000001110
+  110000000
+]
 bitmaps.each do |bitmap|
   Line.create(rows: 3, columns: 3, bitmap: bitmap)
 end
